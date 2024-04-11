496dd1f1346c3be006bb63efac40b609b6a6c21d
all sky <allrobots@163.com>
2024-04-11 20:11:58 +0800

    绕过无法工作的变量代码

--- 脚本/DKTools_Localization提取T++的CSV为JSON/extract_from_csv_transcoding_to_json.py
+++ 脚本/DKTools_Localization提取T++的CSV为JSON/extract_from_csv_transcoding_to_json.py
@@ -2,14 +2,30 @@
 import os
 import csv
 import json
+import re
+
+# 用于匹配无法在译文中工作的变量或其它代码
+separator_regex='\\\\[a-zA-Z]+\[([^\]]*)\]|\\\\\{|\\\\\||if\([^\)]*\)|\n'
 
 """
-提取译文到字典，返回{原文:译文}字典
-同时也分割
+提取译文到字典，返回{原文1:译文1,原文2:译文2...}字典
+由于插件只支持{}换行，不支持内部插入\n、\\N[1]、if(\c[1]>0)等参数
+只好删掉这些参数，比如原文是：
+\\n[1]if(c[0]>0)\\n「　あ、お友達の\\n[3]ちゃんも来たし、今日は学校だからもう　\\c[0]行くね if(v[87]>0)
+译文:
+\\n[1]if(c[0]>0)\\n「　啊,朋友的\\n[3],而且今天在学校也已经\\c[0]去过if(v[87]>0)
+就建立一个对应的字典：
+{
+"「　あ、お友達の": "「　あ、お友達の":"啊,朋友的",
+"ちゃんも来たし、今日は学校だからもう　": "ちゃんも来たし、今日は学校だからもう　":",而且今天在学校也已经",
+"行くね": "行くね":"去过"
+}
+
+但是，如果AI译文没有原文对应的系统参数，插件输出的译文可能会比较难看
 """
 def process_csv_files(directory):
     new_data_dict = {}
-    trans_data_dict={}
+    dict_add_symbols={}
     for filename in os.listdir(directory):
         if filename.endswith(".csv"):
             file_path = os.path.join(directory, filename)
@@ -19,31 +35,30 @@ def process_csv_files(directory):
                 for row in reader:
                     original_text = row.get("Original Text")
                     initial = row.get("Initial")
-
+                    dict_add_symbols.update({original_text:original_text})
                     if initial is None or initial == "":
                         continue
-
-                    if '\n' in original_text:
-                        key_items = original_text.split("\n")
-                        value_items = initial.split("\n")
+                    regex_test=re.search(separator_regex,original_text)
+                    if regex_test:
+                        key_items = re.sub(separator_regex,'\n',original_text).split("\n")
+                        key_items[:]=(value for value in key_items if value != "")
+                        value_items = re.sub(separator_regex,'\n',initial).split("\n")
+                        value_items[:]=(value for value in value_items if value != "")
+                        # 译文JSON
                         for i in range(len(key_items)):
+                            dict_add_symbols[original_text]=dict_add_symbols[original_text].replace(key_items[i],"{"+key_items[i]+"}")
                             try:
                                 new_data_dict[key_items[i]] = value_items[i]
                             except:
                                 new_data_dict[key_items[i]]=""
-                    else:
-                        new_data_dict[original_text] = initial
-                    trans_data_dict[original_text] = initial
-    return trans_data_dict,new_data_dict
 
-"""
-给原文添加{}，\\n换行符前的子字符串添加到{}之间
-"""
-def process_origin_text_add_symbol(dict):
-    new_dict={}
-    for key in dict.keys():
-        new_dict.update({key:"{"+key.replace('\n','}{')+"}"})
-    return new_dict
+                    else:
+                        dict_add_symbols[original_text] = "{" + original_text + "}"
+                        new_data_dict[original_text] = initial
+
+    return dict_add_symbols,new_data_dict
+
+
 
 """
 字典保存成main.json，然后放到locales/zh文件夹就可以了
@@ -108,15 +123,14 @@ if not os.path.exists(new_dir):
     os.mkdir(new_dir)
 output_file_path = os.path.join(new_dir,"main.json")
 save_dict_as_json(json_dict, output_file_path) # 将字典保存为JSON文件
-print(f"\n已保存至\n{output_file_path}\n\n可以移动locales文件夹放至www目录，然后配置多语言翻译插件\n需DKTools_Localization.js")
+print(f"\n已保存至\n{output_file_path}\n\n可以移动locales文件夹放至www目录，然后配置多语言翻译插件\n注：DKTools_Localization.js插件")
 
 
 # 保存T++导入格式CSV
-origin_dict=process_origin_text_add_symbol(trans_dict)
-new_dir1=os.path.join(directory_path,"原文添加双{}","ctrl")
+new_dir1=os.path.join(directory_path,"原文添加双{}")
 if not os.path.exists(new_dir1):
     os.makedirs(new_dir1)
 output_file_path = os.path.join(new_dir1,"T++导入该文件然后覆盖data.csv")
-save_dict_as_csv(origin_dict, output_file_path)
+save_dict_as_csv(trans_dict, output_file_path)
 print(f"\nT++导入\n{output_file_path}\n然后导出到data覆盖，用于多语言插件要求原文格式，否则插件无法加载原文并且翻译")
 
